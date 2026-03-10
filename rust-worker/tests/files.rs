a8bc2f7831c0a1a45407ed4e6bd666c2c7d9144c
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2026-03-10 10:38:44 +0300

    The rest of base tests

--- rust-worker/tests/files.rs
+++ rust-worker/tests/files.rs
@@ -20,9 +20,8 @@ async fn test_get_files() {
     assert!(body.is_array());
 }
 
-// root folder
 #[tokio::test]
-async fn test_create_folder() {
+async fn test_create_folder_wo_parent() {
     let app = spawn_app().await;
 
     let res = app.client
@@ -36,9 +35,66 @@ async fn test_create_folder() {
     assert_eq!(res.status(), 200);
 }
 
-async fn test_upload_file(){}
 
-async fn test_delete_file(){}
+#[tokio::test]
+async fn test_delete_file(){
+    //get this from another func for more control or get files and then rand
+    let file_id = "123";
+    
+    let app = spawn_app().await;
+    let res = app.client
+        .post(format!("{}/delete-file", app.base_url))
+        .json(&serde_json::json!({"owner_id":USER_UUID,
+                                  "file_id":file_id}))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(res.status(), 200);
+}
 
-async fn test_rename_file(){}
+#[tokio::test]
+async fn test_upload_file_wo_parent(){
+
+    let app = spawn_app().await;
+    
+    let file = b"Hello World".to_vec();
+    let form = reqwest::multipart::Form::new()
+        .part("file", reqwest::multipart::Part::bytes(file)
+            .file_name("test.txt")
+            .mime_str("text/plain").unwrap())
+        .text("user_id", USER_UUID)
+        .text("parent_id", "");
+    
+    let res = app.client
+        .post(format!("{}/upload-file", app.base_url))
+        .multipart(form)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(res.status(), 200);
+}
+
+
+#[tokio::test]
+async fn test_rename_file(){
+    
+    //get this from another func for more control or get files and then rand
+    let file_id = "123";
+    
+    let app = spawn_app().await;
+
+    let res = app.client
+        .post(format!("{}/rename-file", app.base_url))
+        .json(&serde_json::json!({"owner_id":USER_UUID,
+                                  "file_id":file_id,
+                                  "file_name": "NewName"}))
+        .send()
+        .await
+        .unwrap();
+    
+    assert_eq!(res.status(), 200);
+}
+
+
 
